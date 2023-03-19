63c06e5c1cec2fb6175cb4bfc65b5cddd39095ae
Gene <eparmon@tuta.io>
2023-03-19 15:17:16 +0400

    Initialize Spring Boot application with DB connection

---
+++ src/main/kotlin/app/mindlikewater/telegrambot/TelegrambotApplication.kt
@@ -0,0 +1,11 @@
+package app.mindlikewater.telegrambot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TelegrambotApplication
+
+fun main(args: Array<String>) {
+	runApplication<TelegrambotApplication>(*args)
+}
---
+++ src/test/kotlin/app/mindlikewater/telegrambot/TelegrambotApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.mindlikewater.telegrambot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TelegrambotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
